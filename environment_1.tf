28722a66f999dfdc5e2d54d99329dbc68728b32f
Filip <45733068+filipknapik@users.noreply.github.com>
2022-08-11 20:01:00 +0200

    first script created

---
+++ environment_1.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project = "*************"
+  region = "us-central1"
+}
+
+
+resource "google_composer_environment" "tf_created" {
+  name   = "tfcreated"
+  region = "us-central1"
+  config {
+    software_config {
+        image_version = "composer-2.0.22-airflow-2.2.5"
+    }
+    
+    env_variables = {
+        ENVIRONMENT_CODE_SCRIPT = "https://github.com/filipknapik/composer_test_env/blob/main/environment_1.tf"
+    }
+
+  
+}
